911bdc6772683df917425ab260aee3c3858bbb1f
Dario Pepić <pepy253@gmail.com>
2021-01-19 22:08:12 +0100

    Initial commit

---
+++ Button spawn/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Button_spawn
+{
+    partial class ButtonSpawn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UserInput = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // UserInput
+            // 
+            this.UserInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.UserInput.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.UserInput.Location = new System.Drawing.Point(10, 30);
+            this.UserInput.MaxLength = 2;
+            this.UserInput.Name = "UserInput";
+            this.UserInput.Size = new System.Drawing.Size(300, 20);
+            this.UserInput.TabIndex = 0;
+            this.UserInput.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.UserInput_KeyPress);
+            this.UserInput.KeyUp += new System.Windows.Forms.KeyEventHandler(this.UserInput_KeyUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Input a number to spawn buttons up to 50";
+            // 
+            // ButtonSpawn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(319, 61);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.UserInput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(335, 1035);
+            this.Name = "ButtonSpawn";
+            this.Text = "Button Spawn";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UserInput;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Button spawn/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Button_spawn
+{
+    public partial class ButtonSpawn : Form
+    {
+        List<Button> buttons;
+
+        public ButtonSpawn()
+        {
+            InitializeComponent();
+
+            buttons = new List<Button>();
+        }
+
+        private void UserInput_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsDigit(e.KeyChar) || (e.KeyChar == (char)Keys.Back)))
+            {
+                e.Handled = true;
+            }
+           
+        }
+
+        private void UserInput_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter && !string.IsNullOrWhiteSpace(UserInput.Text) && Convert.ToInt32(UserInput.Text) <= 50)
+            {
+                MakeBtnList(Convert.ToInt32(UserInput.Text));
+
+                this.Size = new Size(335, 100 + (buttons[0].Size.Height) *(Convert.ToInt32(Math.Ceiling(Convert.ToDouble(UserInput.Text) / 3))));
+
+                for (int i = 0; i < buttons.Count; i++)
+                    this.Controls.Add(buttons[i]);
+
+                e.Handled = true;
+            }
+        }
+
+        private void BtnClick(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+
+            MessageBox.Show("You have clicked " + button.Name);
+        }
+
+        void MakeBtnList(int input)
+        {
+            int btnWidth = 100;
+            int btnHight = 55;
+            int widthOffset = 10;
+
+            if (input > buttons.Count)
+            {
+                for (int i = buttons.Count(); i < input; i++)
+                {
+                    Button b = new Button
+                    {
+                        Size = new Size(btnWidth, btnHight),
+                        Name = "Button " + (i + 1),
+                        Text = "Button " + (i + 1),
+                        Location = new Point((btnWidth * (i % 3)) + widthOffset, btnHight + btnHight * Convert.ToInt32(Math.Floor(i / 3.0)))
+                    };
+
+                    b.Click += (s,e)=>{ MessageBox.Show(((Button)s).Text); };
+
+                    buttons.Add(b);
+                }
+            }
+            else if (input < buttons.Count)
+            {
+                for(int i = buttons.Count - 1; i > input - 1; i--)
+                {
+                    Controls.Remove(buttons[i]);
+                    buttons.RemoveAt(buttons.Count - 1);
+                }
+            }
+        }
+
+        private void B_Click(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
